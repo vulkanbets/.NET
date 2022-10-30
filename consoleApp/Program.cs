3683327efaa36de7a65cf9c359f18074ea680d3e
Jabberwocky <mrfpgapro@gmail.com>
2022-10-29 17:03:15 -0700

    Command Line arguments using .NET 6 Console App

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -4,7 +4,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string arg_One = Environment.GetCommandLineArgs()[1];
+        string arg_Two = Environment.GetCommandLineArgs()[2];
+        Console.WriteLine("\n\nHello From .NET 6!");
+        Console.WriteLine("\nCommand Line argument 1  ==  {0}", arg_One);
+        Console.WriteLine("\nCommand Line argument 2  ==  {0}\n\n", arg_Two);
     }
 }
 
---
+++ consoleApp/README.md
@@ -0,0 +1,3 @@
+## Instructions to run this app
+- cd into App directory
+- dotnet run "**First Command Line Argument**" "**Second Command Line Argument**" etc.

55d035587765fc44adaf051205b94f7dc98a8e82
Jabberwocky <mrfpgapro@gmail.com>
2022-10-29 17:19:47 -0700

    Handling exceptions.

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -4,11 +4,21 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string arg_One = Environment.GetCommandLineArgs()[1];
-        string arg_Two = Environment.GetCommandLineArgs()[2];
-        Console.WriteLine("\n\nHello From .NET 6!");
-        Console.WriteLine("\nCommand Line argument 1  ==  {0}", arg_One);
-        Console.WriteLine("\nCommand Line argument 2  ==  {0}\n\n", arg_Two);
+        try
+        {
+            string arg_One = Environment.GetCommandLineArgs()[1];
+            string arg_Two = Environment.GetCommandLineArgs()[2];
+            Console.WriteLine("\n\nHello From .NET 6!");
+            Console.WriteLine("\nCommand Line argument 1  ==  {0}", arg_One);
+            Console.WriteLine("\nCommand Line argument 2  ==  {0}\n\n", arg_Two);
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine("\n\nException Trown  ==  {0}", ex);
+        }
+
+        Console.WriteLine("\n\n\nEnd of Program...\n\n");
+
     }
 }
 
